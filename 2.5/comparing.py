141c698f20d44ccab0c047d44e642d4cb28faab2
JaydenWong754 <1530117@learn.vsb.bc.ca>
2025-09-29 22:05:01 +0000

    hi

--- 2.5/comparing.py
+++ 2.5/comparing.py
@@ -6,3 +6,39 @@ You must use user input to add interactivity to the program.
 You must design your algorithm in English first, then translate it to Python code.
 Test as you go! Describe in your comments what steps you took to test your code.
 """
+score = 0
+userline = ""
+userline2 = ""
+
+file = open("2.4/responses.csv")
+
+print("Hello, Whats your Name!")
+
+reply = input().lower().strip()
+
+for line in file:
+    if reply in line.lower():
+        print("Heres your info.")
+        print(line)
+        userline = line
+
+print("Who would you like to compare yourself to?")
+
+reply2 = input().lower().strip()
+
+for line2 in file:
+    if reply2 in line2.lower():
+        print("Heres their info.")
+        print(line2)
+        userline2 = line2
+
+userline = userline.split(",")
+
+userline2 = userline2.split(",")
+
+userline = list(userline)
+
+userline2 = list(userline2)
+
+print(userline)
+print(userline2)
