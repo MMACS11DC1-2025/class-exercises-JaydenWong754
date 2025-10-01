57b091dfff731b63ae5a6da2657a149fd2260b40
JaydenWong754 <1530117@learn.vsb.bc.ca>
2025-10-01 21:29:39 +0000

    h

--- 2.5/comparing.py
+++ 2.5/comparing.py
@@ -6,17 +6,19 @@ You must use user input to add interactivity to the program.
 You must design your algorithm in English first, then translate it to Python code.
 Test as you go! Describe in your comments what steps you took to test your code.
 """
-score = 0
+
 userline = ""
 userline2 = ""
 
 file = open("2.4/responses.csv")
 
+all_lines = file.readlines()
+
 print("Hello, Whats your Name!")
 
 reply = input().lower().strip()
 
-for line in file:
+for line in all_lines:
     if reply in line.lower():
         print("Heres your info.")
         print(line)
@@ -26,7 +28,7 @@ print("Who would you like to compare yourself to?")
 
 reply2 = input().lower().strip()
 
-for line2 in file:
+for line2 in all_lines:
     if reply2 in line2.lower():
         print("Heres their info.")
         print(line2)
@@ -36,9 +38,24 @@ userline = userline.split(",")
 
 userline2 = userline2.split(",")
 
-userline = list(userline)
+#userline = list(userline)
 
-userline2 = list(userline2)
+#userline2 = list(userline2)
 
-print(userline)
-print(userline2)
+result = [item for item in userline if item in userline2]
+
+total = len(result)
+
+percent = total / len(userline) * 100 
+
+print("You have " + str(result) + " in common and are " + str(percent) + " percent alike")
+
+print("Do you wanna see who your most likely to be friends with")
+
+reply3 = input().lower().strip()
+
+if reply3 == "yes" or "yeah":
+    print("ok!")
+
+else:
+    print("umm rude")
