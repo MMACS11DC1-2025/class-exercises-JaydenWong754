c9a466bae6d61a85b9c67885f992f0fafa089d82
JaydenWong754 <1530117@learn.vsb.bc.ca>
2025-10-28 22:07:41 +0000

    test

--- 4.4/main.py
+++ 4.4/main.py
@@ -10,8 +10,156 @@ def fractal(times):
         turtle.stamp()
 
     else:
+        turtle.goto(2, 0)
         turtle.left(45)
         turtle.forward(30)
         turtle.right(90)
         turtle.forward(30)
+        turtle.right(90)
+        turtle.forward(30)
+        turtle.right(90)
+        turtle.forward(30)
+        turtle.penup()
+        
+        turtle.goto(-10, 17)
+        turtle.pendown()
+        turtle.forward(30)
+        turtle.right(90)
+        turtle.forward(30)
+        turtle.right(90)
+        turtle.forward(30)
+        turtle.right(90)
+        turtle.forward(30)
+        turtle.penup()
+        
+        turtle.goto(70, 35)
+        turtle.pendown()
+        turtle.forward(30)
+        turtle.right(90)
+        turtle.forward(30)
+        turtle.right(90)
+        turtle.forward(30)
+        turtle.right(90)
+        turtle.forward(30)
+        turtle.penup()
+        
+        turtle.goto(22, 88)
+        turtle.pendown()
+        turtle.forward(30)
+        turtle.right(90)
+        turtle.forward(30)
+        turtle.right(90)
+        turtle.forward(30)
+        turtle.right(90)
+        turtle.forward(30)
+        turtle.penup()
+
+        turtle.goto(22, 36)
+        turtle.setheading(90)
+        turtle.pendown()
+        turtle.stamp()
+fractal(10)
+
+import turtle
+
+turtle = turtle.Turtle()
+
+cords = {
+    "second": [-10, 17],
+    "third": [70, 35],
+    "fourth": [22, 36]
+}
+
+
+def fractal(times):
+
+    if times == 0:
+        turtle.stamp()
+
+    else:
+          turtle.goto(-1, 0)
+					#turtle.left(45)
+          turtle.pendown()
+          turtle.forward(30)
+          turtle.right(90)
+          turtle.forward(30)
+          turtle.right(90)
+          turtle.forward(30)
+          turtle.right(90)
+          turtle.forward(30)
+          turtle.penup()
+idk()
+
+turtle.goto(22, 36)
+turtle.setheading(90)
+turtle.pendown()
+turtle.stamp()
+
+
+def idk():
+	for key in cords:
+	  #turtle.goto(cords[key][0], cords[key][1])
+	  turtle.pendown()
+	  turtle.forward(30)
+	  turtle.right(90)
+		turtle.forward(30)
+		turtle.right(90)
+		turtle.forward(30)
+		turtle.right(90)
+		turtle.forward(30)
+		turtle.penup()
+        
+
+fractal(10)
+
+import turtle
+
+turtle = turtle.Turtle()
+
+cords = {
+    "second": [-10, 17],
+    "third": [70, 35],
+    "fourth": [22, 36]
+}
+
+
+def fractal(times):
+
+    if times == 0:
+        turtle.stamp()
+
+    else:
+          turtle.goto(-1, 0)
+					turtle.left(45)
+          turtle.pendown()
+          turtle.forward(30)
+          turtle.right(90)
+          turtle.forward(30)
+          turtle.right(90)
+          turtle.forward(30)
+          turtle.right(90)
+          turtle.forward(30)
+          turtle.penup()
+idk()
+
+turtle.goto(22, 36)
+turtle.setheading(90)
+turtle.pendown()
+turtle.stamp()
+
+
+def idk()
+	for key in cords:
+		turtle.goto(cords[key][0], cords[key][1]
+    turtle.pendown()
+    turtle.forward(30)
+		turtle.right(90)
+		turtle.forward(30)
+		turtle.right(90)
+		turtle.forward(30)
+		turtle.right(90)
+		turtle.forward(30)
+		turtle.penup()
+        
+
 fractal(10)
