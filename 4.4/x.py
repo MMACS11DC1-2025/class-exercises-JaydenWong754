5fc8a52ff495d0338bdc58a1be35bb042192fb05
JaydenWong754 <1530117@learn.vsb.bc.ca>
2025-10-31 22:03:27 +0000

    zx

--- 4.4/x.py
+++ 4.4/x.py
@@ -5,22 +5,29 @@ def namebot():
     letters = []
     letters2 = []
     for x in name:
-        if x in ["aeiou"]:
+        if x in "aeiou":
             letters += [x]
             letters2 += [x]
+            print(letters)
+            print(letters2)
 
     for y in letters:
         letters[0] = letters[-1]
         letters2[-1] = letters[0]
+        print(letters)
+        print(letters2)
+
+
 
     for z in name:
-        if z in ["aeiou"]:
-            z = letters[0]
+        if z in "aeiou":
+            z = letters2[0]
             break
 
     for a in range(len(name)):
-        if name[-a] in ["aeiou"]:
+        if name[-a] in "aeiou":
             a == letters2[-1]
+
             break
 
     print(name)
