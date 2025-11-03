30bae499b4e378ef6290ffe693c87c8212a9e43f
JaydenWong754 <1530117@learn.vsb.bc.ca>
2025-11-03 22:53:32 +0000

    happy

--- 4.4/main.py
+++ 4.4/main.py
@@ -2,24 +2,39 @@ import turtle
 
 t = turtle.Turtle()
 
-max_steps = 60
+t.speed('fastest')
+
+
+
+print("Make a star! How big is it?")
+
+max_steps = int(input())
+
+print("what colors?")
 
 def recursive_spiral(current_step, max_steps):
+    palette = {"blue": "#2ABCFF", "pink": "#FF1ED6", "green": "#28FC4B"}
+
     if current_step >= max_steps:
         return
-    
+
     base_length = 30
     length = base_length + (current_step * 10)
-
+    
+    t.color(palette["blue"])
     t.forward(length)
-    t.right(90)
+    t.right(190)
+    t.color(palette["pink"])
     t.forward(length)
-    t.right(90)
+    t.right(190)
+    t.color(palette["green"])
     t.forward(length)
-    t.right(90)
+    t.right(190)
 
     recursive_spiral(current_step + 1, max_steps)
-
+    
 t.penup()
 t.goto(-10, -10)
 t.pendown()
+
+recursive_spiral(0, max_steps)
