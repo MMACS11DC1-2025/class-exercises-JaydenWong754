7ba8eb83f15e3a95671b22af368d924ca044bb4f
JaydenWong754 <1530117@learn.vsb.bc.ca>
2025-11-07 17:16:45 +0000

    d

--- 4.4/igiveup.py
+++ 4.4/igiveup.py
@@ -1,16 +1,77 @@
-import turtle
+'''
+def biggest(list):
+    bignum = list[0]
+    for x in list:
+        if x > bignum:
+            bignum = x
+    return bignum
 
-t = turtle.Turtle()
+list = [1, 2, 3, 4]
 
-def fractal(times):
-    if times == 0:
-        return
+print(biggest(list))
+'''
+'''
+titles = []
+count = 1
+counter = 0
+print("how many books")
+amt = input()
+finalcount = 0
+
+for x in range(int(amt)):
+    print("title " + str(count) + " ?")
+    title = input().strip().lower()
+    titles += title
+    count += 1
+
+booktitle = True
+
+while booktitle == True:
+    print("max title length?")
+    maxwords = int(input())
+
+    if maxwords == " ":
+        booktitle = False
     
-    for x in range(200):
-        t.forward()
-        t.forward(30)
-        t.right(90)
-        t.forward(30)
-        t.right(90)
-        t.forward(30)
-        t.right(90)
+    else:
+        for y in titles:
+            y.split()
+            for z in y.split():
+                counter += 1
+                if counter < maxwords:
+                    finalcount += 1
+    print("theres " + str(finalcount) + " book with under " + str(maxwords) + " words")
+'''
+
+name = []
+vowel = []
+print("give a name")
+name = input().split()
+newname = name
+for x in name:
+    if x in "aeiou":
+        vowel += x
+        for
+        
+
+"for i in range(len(vowel)):"
+    "if x in "":"
+        "x = vowel[-1 - i]"
+
+
+'''
+import random
+words = random.choice(["lose", "cats", "fart"])
+chars = len(words)
+x = True
+while x == True:
+    print("guess a letter")
+    guess = input().strip().lower()
+    for y in range(chars):
+        if guess == words[y]:
+            print(str(guess) + "is the " +str(y + 1) + "letter out of " + str(chars) )
+
+    if guess == words:
+        print("u got it! it was " + words)
+        x = False
+'''
