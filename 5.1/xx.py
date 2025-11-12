195703c8730493f2555f5e8ca63dae6323842cb9
JaydenWong754 <1530117@learn.vsb.bc.ca>
2025-11-12 21:30:39 +0000

    hhs

---
+++ 5.1/xx.py
@@ -0,0 +1,18 @@
+def color(r, g, b):
+    if r >= 230 and r <= 255 and g >= 0 and g <= 25 and b >= 0 and b <= 25:
+        print("blue")
+
+    if r >= 0 and r <= 25 and g >= 230 and g <= 255 and b >= 0 and b <= 25:
+        print("green")
+
+    if r >= 0 and r <= 25 and g >= 0 and g <= 25 and b >= 230 and b <= 255:
+        print("blue")
+
+    if r >= 230 and r <= 255 and g >= 0 and g <= 25 and b >= 0 and b <= 25:
+        print("blue")
+
+r = 10
+
+g = 250
+
+b = 0
