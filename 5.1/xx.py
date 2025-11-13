2837af4e79feccc4fe856ff1884884273c226ac9
JaydenWong754 <1530117@learn.vsb.bc.ca>
2025-11-13 20:29:18 +0000

    c

--- 5.1/xx.py
+++ 5.1/xx.py
@@ -1,12 +1,11 @@
 def color(r, g, b):
     if r >= 230 and r <= 255 and g >= 0 and g <= 25 and b >= 0 and b <= 25:
-        print("red")
+        return "red"
 
     if r >= 0 and r <= 25 and g >= 230 and g <= 255 and b >= 0 and b <= 25:
-        print("green")
+        return "green"
 
     if r >= 0 and r <= 25 and g >= 0 and g <= 25 and b >= 230 and b <= 255:
-        print("blue")
+        return "blue"
 
-
-    print(color(5, 235, 2))
+print((color(5, 235, 2)))
