66b70a3bbd9e2eeb48bff11eeff48264d71f4bac
JaydenWong754 <1530117@learn.vsb.bc.ca>
2025-11-14 21:21:55 +0000

    ;

--- 5.1/xx.py
+++ 5.1/xx.py
@@ -1,11 +1,6 @@
 def isgreen(r, g, b):
-    if r >= 230 and r <= 255 and g >= 0 and g <= 25 and b >= 0 and b <= 25:
-        return "red"
-
     if r >= 0 and r <= 25 and g >= 230 and g <= 255 and b >= 0 and b <= 25:
-        return "green"
+        return True
 
-    if r >= 0 and r <= 25 and g >= 0 and g <= 25 and b >= 230 and b <= 255:
-        return "blue"
-
-print((isgreen(5, 235, 2)))
+    else:
+        return False
